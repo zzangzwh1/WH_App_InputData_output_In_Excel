334c8a0f8aa42e25d136441f66caa07812a6e612
Wonhyuk Cho <zzangzwh@hotmail.com>
2023-04-28 17:13:37 -0600

    simple app for Employe management

---
+++ WH_App_InputData_output_In_Excel/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace WH_App_InputData_output_In_Excel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this._idText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this._nameText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this._salary = new System.Windows.Forms.TextBox();
+            this._addBtn = new System.Windows.Forms.Button();
+            this._show = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this._caldenr = new System.Windows.Forms.DateTimePicker();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Employee ID";
+            // 
+            // _idText
+            // 
+            this._idText.Location = new System.Drawing.Point(28, 166);
+            this._idText.Multiline = true;
+            this._idText.Name = "_idText";
+            this._idText.Size = new System.Drawing.Size(237, 36);
+            this._idText.TabIndex = 1;
+          
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Emplyee Name";
+            // 
+            // _nameText
+            // 
+            this._nameText.Location = new System.Drawing.Point(28, 251);
+            this._nameText.Multiline = true;
+            this._nameText.Name = "_nameText";
+            this._nameText.Size = new System.Drawing.Size(237, 36);
+            this._nameText.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 310);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Date Start";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 403);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Salary";
+            // 
+            // _salary
+            // 
+            this._salary.Location = new System.Drawing.Point(28, 436);
+            this._salary.Multiline = true;
+            this._salary.Name = "_salary";
+            this._salary.Size = new System.Drawing.Size(237, 36);
+            this._salary.TabIndex = 7;
+            // 
+            // _addBtn
+            // 
+            this._addBtn.Location = new System.Drawing.Point(28, 504);
+            this._addBtn.Name = "_addBtn";
+            this._addBtn.Size = new System.Drawing.Size(100, 41);
+            this._addBtn.TabIndex = 8;
+            this._addBtn.Text = "Add";
+            this._addBtn.UseVisualStyleBackColor = true;
+            // 
+            // _show
+            // 
+            this._show.Location = new System.Drawing.Point(165, 504);
+            this._show.Name = "_show";
+            this._show.Size = new System.Drawing.Size(100, 41);
+            this._show.TabIndex = 9;
+            this._show.Text = "Show";
+            this._show.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.YellowGreen;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(4, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(830, 100);
+            this.panel1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(198, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(423, 44);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Employee Management";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(290, 166);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(532, 306);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // _caldenr
+            // 
+            this._caldenr.Location = new System.Drawing.Point(28, 355);
+            this._caldenr.Name = "_caldenr";
+            this._caldenr.Size = new System.Drawing.Size(237, 22);
+            this._caldenr.TabIndex = 13;
+           
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 581);
+            this.Controls.Add(this._caldenr);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this._show);
+            this.Controls.Add(this._addBtn);
+            this.Controls.Add(this._salary);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this._nameText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this._idText);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox _idText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox _nameText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox _salary;
+        private System.Windows.Forms.Button _addBtn;
+        private System.Windows.Forms.Button _show;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker _caldenr;
+    }
+}
+
---
+++ WH_App_InputData_output_In_Excel/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace WH_App_InputData_output_In_Excel
+{
+    public partial class Form1 : Form
+    {
+        public string connectionString = "Data Source=(localdb)\\Local;Initial Catalog=EmployeeManagement;Integrated Security=True";
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+           
+            this.Text = $"Insert into DB and Get Result in Excel";
+
+            _idText.TextChanged += _idText_TextChanged;
+            _salary.TextChanged += _salary_TextChanged;
+            _nameText.TextChanged += _nameText_TextChanged;
+            _addBtn.Click += _addBtn_Click;
+            _show.Click += _show_Click;
+           
+            
+
+        }
+
+      
+
+        private void _show_Click(object sender, EventArgs e)
+        {
+           
+            dataGridView1.DataSource = null;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Employee";
+
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                {
+                    dt.Clear();
+                    adapter.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                }
+            }
+        }
+
+        private void _nameText_TextChanged(object sender, EventArgs e)
+        {
+            if (!Regex.IsMatch(_nameText.Text, "^[a-zA-Z]+$"))
+            {
+                MessageBox.Show("Please enter only Letter characters.");
+                _nameText.Text = "";
+            }
+        }
+
+        private void _salary_TextChanged(object sender, EventArgs e)
+        {
+            if (!Regex.IsMatch(_salary.Text, "^[0-9]+$"))
+            {
+                MessageBox.Show("Please enter only numeric characters.");
+                _salary.Text = "";
+            }
+        }
+
+
+
+
+
+        private void _idText_TextChanged(object sender, EventArgs e)
+        {
+            if (!Regex.IsMatch(_idText.Text, "^[0-9]+$"))
+            {
+                MessageBox.Show("Please enter only numeric characters.");
+                _idText.Text = "";
+            }
+        }
+
+
+
+        private void _addBtn_Click(object sender, EventArgs e)
+        {
+            // Define the query to insert data into a table
+            string query = "INSERT INTO Employee (ID, name, startDate, salary) VALUES (@ID, @name, @startDate,@salary)";
+
+            // Open a connection to the database
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                // Create a command object
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    // Set the parameter values
+
+                    command.Parameters.AddWithValue("@ID", _idText.Text);
+                    command.Parameters.AddWithValue("@name", _nameText.Text);
+                    command.Parameters.AddWithValue("@startDate", _caldenr.Value);
+                    command.Parameters.AddWithValue("@salary", _salary.Text);
+
+                    int rowAResult = command.ExecuteNonQuery();
+
+                    Console.WriteLine($"Affected Line : {rowAResult}");
+
+
+
+                }
+            }
+
+        }
+
+
+
+
+    }
+}
